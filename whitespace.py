fb0059d3a7e36b72d825be7e72f82f9a9a743ec9
Greg Schuman <gcschuman@hotmail.com>
2020-10-07 19:34:02 -0700

    Added file that covers whitespace.

---
+++ whitespace.py
@@ -0,0 +1,10 @@
+print("Python")
+
+#Added whitepace
+print("\tPython")
+
+#Added lines in a string
+print("Rick and Morty characters:\nSummer\nScary Terry\nKrombopulous Michael")
+
+#Added lines in a string with whitespace
+print("Pokemon:\n\tSnubbull\n\tPsyduck\n\tKoffing")
