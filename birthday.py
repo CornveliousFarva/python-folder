f41cd170abbc2b8a4fa57252771442e4657fe561
Greg Schuman <gcschuman@hotmail.com>
2020-10-07 20:46:15 -0700

    Added birthday file.

---
+++ birthday.py
@@ -0,0 +1,9 @@
+#Birthday message with type error
+# age = 31 
+# message = "Happy " + age + "st birthday!"
+# print(message)
+
+#Birthday message correctly
+age = 31
+message = "Happy " + str(age) + "st birthday"
+print(message)
