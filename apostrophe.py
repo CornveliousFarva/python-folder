1dae48ec205caa9a45ae63292fa800e7935ece52
Greg Schuman <gcschuman@hotmail.com>
2020-10-07 20:32:50 -0700

    Added case name file.

---
+++ apostrophe.py
@@ -0,0 +1,7 @@
+#Regular message
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+#Regular message but with a syntax error
+# message = 'I'm the danger!'
+# print(message)
---
+++ name_cases.py
@@ -0,0 +1,27 @@
+#Exercise 2-3
+name = "Tai"
+message = "Hello, " + name.title() + " are you watching the debate?"
+print(message)
+
+#Exercise 2-4
+name = "Hans Gruber"
+print (name.title())
+print (name.lower())
+print (name.upper())
+
+#Exercise 2-5
+print("Alan Kulwicki once said 'In life, it's best to have a game plan, then do your best to stick to it.'")
+
+#Exercise 2-6
+name = "Thomas Merton"
+message = "perhaps I am stronger than I think."
+print(message)
+
+#Exercise 2-7
+name = ' Scary Terry '
+name.rstrip()
+print(name)
+name.lstrip()
+print(name)
+name.strip()
+print(name)
