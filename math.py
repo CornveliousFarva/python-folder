9d59fa1f5db4e0b4fe4170f5dbbcff460aec72af
Greg Schuman <gcschuman@hotmail.com>
2020-12-14 20:44:19 -0800

    Added math and input files.

---
+++ math.py
@@ -0,0 +1,11 @@
+# Addition
+print(279.99 + 39.57)
+
+# Subtraction
+print(24560 - 11880)
+
+# Multplication
+print(500 * 3)
+
+# Division
+print(1500 / 12)
--- shape.py
+++ shape.py
@@ -1,8 +1,10 @@
+# Right triangle
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
 
+# Square
 print("-----")
 print("|   |")
 print("-----")
