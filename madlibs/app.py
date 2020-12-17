cebea6b411ef48efbed9ef1f8523612e6212c98e
Greg Schuman <gcschuman@hotmail.com>
2020-12-16 19:14:59 -0800

    Made a little mad libs game.

---
+++ madlibs/app.py
@@ -0,0 +1,17 @@
+# color variable
+color = input("Enter a color:")
+
+# plural variable
+plural = input("Enter a plural noun:")
+
+# food variable
+food = input("Enter a food:")
+
+# celebrity variable
+celebrity = input("Enter a celebrity:")
+
+print("Roses are " + color)
+print(plural+ " are blue")
+print(food+ " is sweet")
+print("and so is " +celebrity)
+
