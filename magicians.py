764d1f75f86af5863923240f872ef7628357b7ea
Greg Schuman <gcschuman@hotmail.com>
2021-02-12 11:19:59 -0800

    Added another exercise.

---
+++ magicians.py
@@ -0,0 +1,5 @@
+magicians = ['ash', 'misty', 'brock']
+for magician in magicians:
+    print(magician)
+
+# Breakdown: 'for magician in magicians:' is telling python to retrieve the list of magicians and store it in the variable magician.
