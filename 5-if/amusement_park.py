78f78f337d3b34a7fb0fdd25ccc8f78ca02e9314
Greg Schuman <gcschuman@hotmail.com>
2021-02-19 09:58:59 -0800

    Added examples of if-elif-else statements.

---
+++ 5-if/amusement_park.py
@@ -0,0 +1,12 @@
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0")
+
+elif age < 18:
+    print("Your admission cost is $5.")
+
+else:
+    print("Your admission cost is $10")
+
+# This is an example of if-elif-else syntax. Many real-world situations involve more than 2 possible conditions, such as this one involving amusement park prices
