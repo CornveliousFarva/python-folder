d182827ff0729dd55ab2f29da55c0d740a3ba361
Greg Schuman <gcschuman@hotmail.com>
2021-02-19 10:58:56 -0800

    Added some examples for chapter 6.

---
+++ ch-6-dictionaries/favorite_languages.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c++',
+    'edward': 'javascript',
+    'al': 'ruby'
+}
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + 
+    
+        language.title() + ".")
