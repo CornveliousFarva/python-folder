fd85b1466b37f93aa84a1b38920fce7a713eb54d
Greg Schuman <gcschuman@hotmail.com>
2021-02-20 19:37:09 -0800

    Added chapter 7 stuff.

--- 5-if/banned_users.py
+++
@@ -1,7 +0,0 @@
-banned_users = ['katie','phil', 'trish', 'daryl', 'megan', 'marti', 'cindy', 'kat']
-user = 'jessica'
-
-if user not in banned_users:
-    print(user.title() + ", you can post a response if you wish.")
-
-# This is to check whether a value is not in a list. Line 1 is a list of banned users. Line 2 is a new user. Since Jessica isn't in the list of banned users, she is invited to post a response.
--- 5-if/toppings.py
+++
@@ -1,6 +0,0 @@
-# To determine whether two values aren't equal, you can combine an exclamation point and an equals sign (!=). The exclamation point represents not.
-
-requested_topping = 'mushrooms'
-
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
